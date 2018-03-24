c7cb5eafb01ac9bc334b3682515c4da14937e280
JN646 <josh.ginn@hotmail.co.uk>
2018-03-24 21:36:53 +0000

    Various Changes + Custom CSS

--- views/pages/about.php
+++ views/pages/about.php
@@ -1,13 +1,13 @@
 <!-- Header -->
-<?php include 'views/partials/_header.php';?>
+<?php include '../../views/partials/_header.php';?>
 <!-- DB Config -->
-<?php include 'config/db_config.php';?>
+<?php include '../../config/db_config.php';?>
 <body>
   <div class="container">
     <!-- Nav -->
-    <?php include 'views/partials/_nav.php';?>
+    <?php include '../../views/partials/_nav.php';?>
     <div class="md-col-12">
-      <h1><?php echo "Hello World!"; ?></h1>
+      <h1>About Page</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
         dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <h2>Header Text 2</h2>
@@ -18,5 +18,5 @@
             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
   <!-- Footer -->
-<?php include 'views/partials/_footer.php';?>
+<?php include '../../views/partials/_footer.php';?>
 </div>
