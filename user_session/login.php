cefc2d728a90a96d99a7d71308a4c251d97ef1e2
JN646 <josh.ginn@hotmail.co.uk>
2018-03-24 21:46:05 +0000

    Login and Register pages

---
+++ user_session/login.php
@@ -0,0 +1,29 @@
+<!-- Header -->
+<?php include '../views/partials/_header.php';?>
+<!-- DB Config -->
+<?php include '../config/db_config.php';?>
+<body>
+  <div class="container">
+    <!-- Nav -->
+    <?php include '../views/partials/_nav.php';?>
+    <div class="md-col-12">
+      <h1>Login</h1>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  <!-- Footer -->
+<?php include '../views/partials/_footer.php';?>
+</div>
