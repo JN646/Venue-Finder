002966bc0d179d30e438e4f32c93d25540c54fc1
JN646 <josh.ginn@hotmail.co.uk>
2018-03-25 16:26:14 +0100

    Index.php Merge Clean

--- index.php
+++ index.php
@@ -4,28 +4,7 @@
 <?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/config/db_config.php"); ?>
 <body>
   <div class="container">
-    <!-- Nav -->
-<<<<<<< HEAD
-    <?php include 'views/partials/_nav.php';?>
-<<<<<<< HEAD
-    <h1><?php echo "Hello World!"; ?></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-<<<<<<< HEAD
-    <h2>Hello Boys</h2>
-=======
-    <h2>Header Text 2</h2>
->>>>>>> 152560f274ff6d3639e0e9019f8393f797285d71
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <h3>Header Text 3</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
-=======
-=======
     <?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_nav.php"); ?>
->>>>>>> 5ac272bc0d78c9c9cf7b2aba6c852174778c6817
     <div class="md-col-12">
       <!-- Jumbotron -->
       <div class="jumbotron">
@@ -56,7 +35,6 @@
         </div>
       </div>
     </div>
->>>>>>> 9cb393d07df8c332d72845a22b2db2e69dfcaea8
   <!-- Footer -->
 <?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_footer.php"); ?>
 </div>
