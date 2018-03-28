f749cf1a787ac7c2ee2ba782489249d8d441643b
JN646 <josh.ginn@hotmail.co.uk>
2018-03-28 20:13:28 +0100

    Added Registration Form
    Talent Registration Form

---
+++ talent/talent_register.php
@@ -0,0 +1,57 @@
+<!-- Header -->
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_header.php"); ?>
+<!-- DB Config -->
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/config/db_config.php"); ?>
+<body>
+  <div class="container-fluid">
+    <?php include($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_nav.php"); ?>
+    <div class="md-col-12">
+
+      <!-- Title -->
+      <h1>Talent Registration</h1>
+
+      <!-- Registration Form -->
+      <form class="" action="index.html" method="post">
+
+        <!-- Name -->
+        <label for="">Name/Act</label>
+        <input type="text" name="" value="">
+        <br>
+
+        <!-- Type -->
+        <label for="">Type of Act</label>
+        <select class="" name="">
+          <option value="">Musician</option>
+          <option value="">Band</option>
+          <option value="">DJ</option>
+          <option value="">Other</option>
+        </select>
+        <br>
+
+        <!-- County -->
+        <label for="">County</label>
+        <input type="text" name="" value="">
+        <br>
+
+        <!-- Postcode -->
+        <label for="">Postcode</label>
+        <input type="text" name="" value="">
+        <br>
+
+        <!-- Distance to Travel -->
+        <label for="">Distance to Travel</label>
+        <input type="text" name="" value="">
+        <br>
+
+        <!-- Terms and conditions -->
+        <p>I accept the terms and conditions.</p>
+        <input type="checkbox" name="" value="">
+
+        <!-- Submit Button -->
+        <button type="submit" name="submit">Submit</button>
+
+      </form>
+    </div>
+  <!-- Footer -->
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_footer.php"); ?>
+</div>
