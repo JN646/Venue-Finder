8e48044db3b373a8f3bdea9892f73e5eef1b02bb
JN646 <josh.ginn@hotmail.co.uk>
2018-03-29 20:48:43 +0100

    Added Database listing

---
+++ template/index.php
@@ -0,0 +1,14 @@
+<!-- Header -->
+<?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_header.php"); ?>
+<!-- DB Config -->
+<?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/config/db_config.php"); ?>
+<body>
+  <div class="container-fluid">
+    <?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_nav.php"); ?>
+    <div class="md-col-12">
+      <!-- Template File -->
+
+    </div>
+  <!-- Footer -->
+<?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_footer.php"); ?>
+</div>
