5dc6748da3896edcf3b471832a271088cf9e2d8c
JN646 <josh.ginn@hotmail.co.uk>
2018-03-30 15:38:41 +0100

    Header and footer code changes.

--- views/partials/_footer.php
+++ views/partials/_footer.php
@@ -4,42 +4,41 @@
 <?php
 class ApplicationVersion
 {
-    const MAJOR = 1;
+    // Define version numbering
+    const MAJOR = 0;
     const MINOR = 2;
-    const PATCH = 3;
+    const PATCH = 2;
 
     public static function get()
     {
+        // Prepare git information to form version number.
         $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
 
+        // Get date and time information.
         $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
         $commitDate->setTimezone(new \DateTimeZone('UTC'));
 
+        // Format all information into a version identifier.
         return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:m:s'));
     }
 }
-
 // Usage: echo 'MyApplication ' . ApplicationVersion::get();
 ?>
 
 <!-- Footer Content -->
 <hr>
 <p class="text-center">Copyright &copy; 2018 Copyright Holder All Rights Reserved.</p>
+
+<!-- Display Version information -->
 <p class="text-center" style='font-size: 80%;'><?php echo 'Version ' . ApplicationVersion::get(); ?></p>
+
+<!-- The Debug navigation bar. -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <ul class="navbar-nav mr-auto">
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo $environment; ?>user_session/login.php">Login</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo $environment; ?>/talent/talent_index.php">Talent</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo $environment; ?>user_session/register.php">Register</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo $environment; ?>/talent/talent_listing.php">Listing</a>
-    </li>
+    <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>user_session/login.php">Login</a></li>
+    <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>/talent/talent_index.php">Talent</a></li>
+    <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>user_session/register.php">Register</a></li>
+    <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>/talent/talent_listing.php">Listing</a></li>
   </ul>
 </nav>
 
--- views/partials/_header.php
+++ views/partials/_header.php
@@ -2,34 +2,38 @@
 <html>
   <head>
     <meta charset="utf-8">
+
+    <!-- Page title -->
     <title>Venue Finder</title>
-    <?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/config/db_var.php"); ?>
+
+    <!-- Server config file -->
+    <?php include($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/config/db_var.php"); ?>
+
+    <!-- Bootstrap file -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Custom CSS file -->
     <link rel="stylesheet" href="<?php echo $environment; ?>/public/css/custom.css">
   </head>
   <body>
 
+  <!-- Nav bar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="<?php echo $environment; ?>index.php">Gig Hub</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
+    <!-- Nav bar content -->
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="<?php echo $environment; ?>index.php">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo $environment; ?>/views/pages/about.php">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo $environment; ?>/views/pages/services.php">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
+        <li class="nav-item active"><a class="nav-link" href="<?php echo $environment; ?>index.php">Home <span class="sr-only">(current)</span></a></li>
+        <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>/views/pages/about.php">About</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>/views/pages/services.php">Services</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
       </ul>
+
+      <!-- Search bar -->
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
