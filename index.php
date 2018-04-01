1de16d012150a5a445971f9670acddbd4badaff4
JN646 <josh.ginn@hotmail.co.uk>
2018-04-01 14:05:04 +0100

    Added something to see page.

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
             <h2 class="text-center">Looking for something to see?</h2>
             <img src="<?php echo $environment; ?>/public/images/smartphone-1.png" alt="Network" class="rounded mx-auto d-block" width=30%>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <a class="btn btn-primary" href="<?php echo $environment; ?>/views/pages/about.php">Learn More</a>
+            <a class="btn btn-primary" href="<?php echo $environment; ?>/views/pages/somethingsee.php">Learn More</a>
           </div>
         </div>
       </div>
---
+++ views/pages/somethingsee.php
@@ -0,0 +1,101 @@
+<!-- Header -->
+<?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_header.php"); ?>
+<!-- DB Config -->
+<?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/config/db_config.php"); ?>
+<body>
+  <div class="container-fluid">
+    <?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_nav.php"); ?>
+    <div class="col-md-12">
+      <!-- Template File -->
+      <div class="row">
+
+        <!-- Header -->
+        <div class="jumbotron col-md-12">
+
+          <!-- Jumbo inner -->
+          <div class="col-md-12">
+            <h1 class='display-4'>Looking for something to see?</h1>
+            <h2>Coming Soon!</h2>
+          </div>
+        </div>
+
+          <!-- Step 1 -->
+          <div class="col-md-12 border">
+            <h1>Step 1</h1>
+            <div class="row">
+              <div class="col-md-4">
+                <h3>Image Holder</h3>
+              </div>
+              <div class="col-md-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+          </div>
+
+          <br>
+
+          <!-- Step 2 -->
+          <div class="col-md-12 border">
+            <h1>Step 2</h1>
+            <div class="row">
+              <div class="col-md-4">
+                <h3>Image Holder</h3>
+              </div>
+              <div class="col-md-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+          </div>
+
+          <br>
+
+          <!-- Step 3 -->
+          <div class="col-md-12 border">
+            <h1>Step 3</h1>
+            <div class="row">
+              <div class="col-md-4">
+                <h3>Image Holder</h3>
+              </div>
+              <div class="col-md-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+          </div>
+
+          <br>
+
+          <!-- Step 4 -->
+          <div class="col-md-12 border">
+            <h1>Step 4</h1>
+            <div class="row">
+              <div class="col-md-4">
+                <h3>Image Holder</h3>
+              </div>
+              <div class="col-md-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+          </div>
+
+          <br>
+
+          <!-- Step 5 -->
+          <div class="col-md-12 border">
+            <h1>Step 5</h1>
+            <div class="row">
+              <div class="col-md-4">
+                <h3>Image Holder</h3>
+              </div>
+              <div class="col-md-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+          </div>
+
+          <br>
+
+      </div>
+    </div>
+  <!-- Footer -->
+<?php include ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/views/partials/_footer.php"); ?>
+</div>
