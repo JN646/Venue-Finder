d003cd5173dec46b19ff855a5075cdc77890f2b0
JN646 <josh.ginn@hotmail.co.uk>
2018-04-04 10:25:10 +0100

    Added MeerkroDB to web app.

--- config/db_config.php
+++ config/db_config.php
@@ -1,4 +1,5 @@
  <?php
+require_once ($_SERVER["DOCUMENT_ROOT"] . "/venue/venue-finder/lib/meekrodb.2.3.class.php");
 
 //Customise this file to match your server configutation. If you are unsure what settings these need to be, then contact you system administrator.
 
@@ -11,4 +12,9 @@ if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 }
+
+// Meekro Setup
+DB::$user = 'root';
+DB::$password = '';
+DB::$dbName = 'venues';
 ?>
