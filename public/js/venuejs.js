88f04d6f108e045b9f7658533d5f6d74b9b8d301
JN646 <josh.ginn@hotmail.co.uk>
2018-04-11 09:56:39 +0100

    venuejs.js added with setup guide.

---
+++ public/js/venuejs.js
@@ -0,0 +1,8 @@
+// Blank JS Template.
+
+// Add this code to the page you want this to load from.
+// Then check the console to see if there is a message.
+
+// <link rel="javascript" href="<?php echo $environment; ?>/public/js/venuejs.js">
+
+console.log('VenueJS Loaded...');
